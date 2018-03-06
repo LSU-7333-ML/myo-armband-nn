91f0cafa9e4bd68cd932813ea89387f6fa2e5358
weilewei <lokwei9@gmail.com>
2018-03-06 17:06:26 -0600

    Add files via upload

--- include/data.py
+++ include/data.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def get_data_set(name="train"):
+def get_data_set(name="test"):
     x = None
     y = None
 
     if name is "train":
-        npzfile = np.load("./data/train_set.npz")
+        npzfile = np.load("./data/train_set_1.npz")
         x = npzfile['x']
         y = npzfile['y']
     elif name is "test":
-        npzfile = np.load("./data_set/test_set.npz")
+        npzfile = np.load("./data/test_set.npz")
         x = npzfile['x']
         y = npzfile['y']
 
